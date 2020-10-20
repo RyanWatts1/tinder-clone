57cb5f6bd35328b5a834f2b1f5c185a2b2e4c2f9
RyanWatts1 <devshop202@yahoo.com>
2020-10-19 23:33:04 -0400

    route and path

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import Header from './Header'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>Lets build a Tinder Clone App </h1>
     <Header/>
-     {/* Header */}
+    <Router>
+    <Switch>
+      <Route path="/chat">
+        <h1>I am Chat Page</h1>
+      </Route>
+      <Route path="/">
+        <h1>I am Home Page</h1>
+      </Route>
+    </Switch>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
+
+{/* Header */}
      {/* Tinder Cards */}
      {/* Buttons below tinder cards */}
 
      {/* Chats screen */}
      {/* Individual chat screen */}
     
-
-    </div>
-  );
-}
-
-export default App;
